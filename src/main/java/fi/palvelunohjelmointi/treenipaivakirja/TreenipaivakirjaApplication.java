5ec8168f5682c35ba498f8c5dff84798db0d0e78
Sanna Jyrinki <AA8206@student.jamk.fi>
2021-10-01 11:53:43 +0300

    first commit

---
+++ src/main/java/fi/palvelunohjelmointi/treenipaivakirja/TreenipaivakirjaApplication.java
@@ -0,0 +1,13 @@
+package fi.palvelunohjelmointi.treenipaivakirja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreenipaivakirjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreenipaivakirjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/palvelunohjelmointi/treenipaivakirja/TreenipaivakirjaApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.palvelunohjelmointi.treenipaivakirja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreenipaivakirjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
